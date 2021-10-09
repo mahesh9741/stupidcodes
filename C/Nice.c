6194c9b64222736a52431c656334170efc83ed3e
mahesh9741 <87569662+mahesh9741@users.noreply.github.com>
2021-10-09 21:41:32 +0530

    Create Nice.c

---
+++ C/Nice.c
@@ -0,0 +1,20 @@
+#include<exam.h> 
+void pain() 
+{ 
+preparation=NULL; 
+if(pass) 
+{ 
+father=NULL; 
+mother=NULL; 
+/*no one cares*/ 
+} 
+if(fail) 
+{ 
+while(1) 
+{ 
+father=scold++; 
+mother=scold++; 
+} 
+} 
+}
+
